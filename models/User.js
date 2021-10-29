9ea5712c9d097b26f855e9214bc722e4c38c7d6c
Bart-15 <brat.tabusao@gmail.com>
2021-10-29 17:01:13 +0800

    Sign up functionality

--- models/User.js
+++ models/User.js
@@ -26,4 +26,5 @@ const UserSchema = new Schema({
 
 const User = mongoose.model('user', UserSchema)
 
+
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,37 @@
 const express = require('express');
-
+const User = require('../models/user')
 const router = express.Router();
+const gravatar = require('gravatar');
+require('../db/mongoose')
+const bcrypt = require('bcryptjs');
+
+
 
 router.post('/api/signup', async (req, res) => {
-    res.json("Hello po")
+
+    // hash the password before saving
+    const hashPassword = await bcrypt.hash(req.body.password, 8)
+
+    //default image
+    const avatar = gravatar.url(req.body.email, {
+            s:'200', //size of image
+            r:'pg', 
+            d:'mm'
+    })
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password:hashPassword,
+        avatar
+    })
+
+    try {
+        await newUser.save()
+        res.json({message: 'success'})
+    } catch (err) {
+        res.status(400).json()
+    }
+
 })
 
 
